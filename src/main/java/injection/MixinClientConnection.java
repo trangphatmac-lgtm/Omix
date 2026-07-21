e0615c2db986719313758f0b42cbf292f6164809
dkWyH <trangphatmac@gmail.com>
2026-07-21 21:09:43 +0800

    ChannelHider

--- src/main/java/injection/MixinClientConnection.java
+++ src/main/java/injection/MixinClientConnection.java
@@ -6,6 +6,7 @@ import cn.remix.ui.screen.impl.proxy.ProxyScreen;
 import cn.remix.util.IMinecraft;
 import cn.remix.util.network.PacketUtil;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.Packet;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,8 +17,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientConnection.class)
 public abstract class MixinClientConnection implements IMinecraft {
 
-    @Inject(method = "send(Lnet/minecraft/network/packet/Packet;)V", at = @At("HEAD"), cancellable = true)
-    private void send(Packet<?> packet, CallbackInfo ci) {
+    @Inject(
+            method = "send(Lnet/minecraft/network/packet/Packet;Lio/netty/channel/ChannelFutureListener;Z)V",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void send(Packet<?> packet, ChannelFutureListener listener, boolean flush, CallbackInfo ci) {
         if (PacketUtil.getPackets().remove(packet)) return;
         if (PacketUtil.isBypassingEvents()) return;
         if (Client.instance == null || Client.instance.getEventManager() == null) return;
@@ -34,7 +39,7 @@ public abstract class MixinClientConnection implements IMinecraft {
         if (replacement != null && replacement != packet) {
             ci.cancel();
             PacketUtil.runWithoutEvents(() -> {
-                ((ClientConnection) (Object) this).send(replacement);
+                ((ClientConnection) (Object) this).send(replacement, listener, flush);
                 return null;
             });
         }
